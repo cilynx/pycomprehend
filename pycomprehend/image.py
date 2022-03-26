e99d3e36d41e6a593d3cddcb2e0c5eb216718403
Randy C. Will <randall.will@gmail.com>
2022-03-26 16:48:36 -0700

    Pixel collections

--- pycomprehend/image.py
+++ pycomprehend/image.py
@@ -4,6 +4,7 @@ import os
 class Image():
     def __init__(self, arg=None):
         self.pil_image = None
+        self.pixels = []
         self.path = None
 
         if arg:
@@ -39,3 +40,38 @@ class Image():
 
     def threshold(self, thresh=127):
         return self.point(lambda p: 255 if p > thresh else 0)
+
+    ###########################################################################
+    # Parse through all pixels to prepare for deeper comprehension
+    ###########################################################################
+
+    def parse_pixels(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                self.pixels.append(Pixel(i, j, self.getpixel((i,j))))
+
+    ###########################################################################
+    # Pixel collections
+    ###########################################################################
+
+    def pixels_by_value(self, value):
+        if not self.pixels:
+            self.parse_pixels()
+        return [pixel for pixel in self.pixels if pixel.value == value]
+
+    @property
+    def black_pixels(self):
+        return self.pixels_by_value((0, 0, 0))
+
+    @property
+    def white_pixels(self):
+        return self.pixels_by_value((255, 255, 255))
+
+    @property
+    def border_pixels(self):
+        return [pixel for pixel in self.pixels if pixel.x == 0 or pixel.x == self.width or pixel.y == 0 or pixel.y == self.height]
+class Pixel():
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
